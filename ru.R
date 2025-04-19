008102259fbedfb4b32f4a2d9b34e9925b6e26b5
sachinkp63 <sachinkumarpatel63@gmail.com>
2025-04-19 19:38:27 +0530

    Update ru.R

--- ru.R
+++ ru.R
@@ -1,1 +1,4 @@
 print("Hello World")
+# how we can run :-
+# ===> go to terminal and press downward then select r terminal and to make R interactive....
+# ==== then select all the code and press "ctrl+enter" 
